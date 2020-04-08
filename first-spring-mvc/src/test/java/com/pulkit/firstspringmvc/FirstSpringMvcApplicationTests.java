b6e24922ef45b14689c0412cfc135c85b2215604
pulkit-rastogi97 <pulkit.rastogi97@gmail.com>
2020-04-09 01:53:43 +0530

    Spring App generated

---
+++ first-spring-mvc/src/test/java/com/pulkit/firstspringmvc/FirstSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulkit.firstspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
