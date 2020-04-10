00ec99733fa2cb2c6af18ad6f07a697e56957d15
Pulkit Rastogi <61677254+pulkit-rastogi97@users.noreply.github.com>
2020-04-11 00:14:36 +0530

    Setted up for using JSP pages

--- first-spring-mvc/src/main/java/com/pulkit/firstspringmvc/FirstSpringMvcApplication.java
+++ first-spring-mvc/src/main/java/com/pulkit/firstspringmvc/FirstSpringMvcApplication.java
@@ -2,9 +2,10 @@ package com.pulkit.firstspringmvc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FirstSpringMvcApplication {
+public class FirstSpringMvcApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringMvcApplication.class, args);
